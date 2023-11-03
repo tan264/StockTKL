75a2676c358d308ed0c84d6fc7f5cd67ebd7dec7
Đặng Tấn <tandang264@gmail.com>
2023-11-03 17:42:29 +0700

    Initial commit

---
+++ Server/StockTKL/src/test/java/com/example/stocktkl/StockTklApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stocktkl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTklApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
