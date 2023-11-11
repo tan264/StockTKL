04f162b87f8d0d8addc3cbec4f6c6c22ef1eca57
Phan Van Tuan <ptuan123987@gmail.com>
2023-11-11 16:57:40 +0700

    add docker-compose

--- Server/StockTKL/src/main/java/com/example/stocktkl/payload/request/LoginRequest.java
+++ Server/StockTKL/src/main/java/com/example/stocktkl/payload/request/LoginRequest.java
@@ -1,6 +1,7 @@
 package com.example.stocktkl.payload.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequest {
+    @NotBlank(message = "username is mandatory")
+    @Size(min = 3, max = 20)
     private String username;
 
+    @NotBlank(message = "password is mandatory")
+    @Size(min = 6, max = 40)
     private String password;
 }
